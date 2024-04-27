dd19fe5cff8b3ea54dbc389e65fffe41decdd634
code-with-rajeev <68783059+code-with-rajeev@users.noreply.github.com>
2024-04-27 20:37:07 +0530

    Remove Duplicates from Sorted Array.cpp added

---
+++ Step 3: Solve Problems on Arrays/Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,18 @@
+//
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int m_i = 0;
+        int mx = nums[0];
+        for (int i = 1 ; i < nums.size() ; i++){
+            if (nums[i]> nums[m_i]){ 
+                mx = nums[i];
+                m_i++;
+                nums[m_i] = mx;
+            }
+
+        }
+        return m_i+1;
+    }
+};
