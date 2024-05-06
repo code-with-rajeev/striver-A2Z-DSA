a5b7bb1589554f995fa0388a0ae358c279aa9481
code-with-rajeev <68783059+code-with-rajeev@users.noreply.github.com>
2024-05-06 19:07:12 +0530

    3Sum.cpp updated

--- Step 3: Solve Problems on Arrays/Step 3.3: Hard/3Sum.cpp
+++ Step 3: Solve Problems on Arrays/Step 3.3: Hard/3Sum.cpp
@@ -1,5 +1,3 @@
-//
-
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
@@ -8,29 +6,25 @@ public:
 
         vector<vector<int>>ans;
         sort(nums.begin(),nums.end());
-        nums.insert(nums.begin(),-100001);
         int size = nums.size();
-        int i = 1 , j = 2, k = size-1;
-        
-        while(i<size-2 && j<k){
-            if(nums[i]==nums[i-1]){
-                i++; j=i+1;k=size-1;// optimization
+        int i = 0 , j = 1, k = size-1;
+
+        while(i<size-2){
+            if(i!=0 && nums[i]==nums[i-1]){
+                i++;// optimization
                 continue;
             }
-            while(i<size-2 && j<k){
+            j=i+1;k=size-1;
+            while(j<k){
                 if(i!=j-1 && nums[j]==nums[j-1]){
                     j++; continue; //optimization
                 }
-
-                if(nums[j]+nums[k]+nums[i]==0){
-                    
-                    vector<int> temp = {nums[i],nums[j],nums[k]};
-                    sort(temp.begin(),temp.end());
-                    ans.push_back(temp);
+                int sum = nums[j]+nums[k]+nums[i];
+                if(sum==0){
+                    ans.push_back({nums[i],nums[j],nums[k]});
                     j++;
-                    k=size-1;
                 }
-                if(nums[j]+nums[k]+nums[i]>0){
+                else if(sum>0){
                     k--;
                 }
                 else{ 
